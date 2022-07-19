1cbecbc49241a7f2ac7569ceaf1059021a0f89f9
AMAN-GUPTA-6 <amanhanu003@gmail.com>
2022-07-20 00:24:02 +0530

    .

---
+++ static/scripts/responses.js
@@ -0,0 +1,73 @@
+function getBotResponse(input) {
+    // Simple responses
+  if (input == "hello") 
+  {
+      return "Hello there!";
+
+  } else if (input == "Hello") {
+      return "Hello there!";
+
+
+  } else if (input == "Hello ") {
+      return "Hello there!";
+  
+
+  } else if (input == "hello ") {
+      return "Hello there!";
+
+    } else if (input == "ME! Provide the details of aman") {
+        return "<b>Name :-</b> Aman <br> <b>D.O.B :-</b> 12-11-2003 <br> <b>AGE :-</b> 18 <br> <b>ID :-</b> 212098 <br><b>HEALTH LEAVE :-</b> 1 Day <br><b>PERFORMANCE :-<b/> 98% 4.5 ⭐  ⭐⭐⭐";
+
+
+    } else if (input == "Ok! Thankyou!") {
+        return "Is there any thing else, I can help you with?";
+
+    } else if (input == "ME! Provide the health details of janbhi on 06-07-2022") {
+        return "Janbhi was <b>Healthy and Energetic</b> on 06-07-2022";
+
+
+    } else if (input == "ME! Provide the health details of shreya on 01-07-2022") {
+        return "Shreya was <b>Ill and Unhealthy </b> on 01-07-2022";
+
+
+    } else if (input == "ME! Provide the health details of bobo on 03-07-2022") {
+        return "Bobo was <b>Ill and Unhealthy </b> on 03-07-2022";
+
+
+    } else if (input == "ME! who is top performer of this week?") {
+        return "1. AMAN <br> 2. MICKY <br> 3. JANBHI";
+
+
+    } else if (input == "ME! who is weak performer of this week?") {
+        return "6. SHIV <br> 5. SHREYA <br> 4. BOBO";
+
+
+    }  else if (input == "ME! why shiv is weak performer of this week?") {
+        return "Due to 5 Days, <b>HEALTH LEAVE!</b>";
+
+
+    } else if (input == "reason?") {
+        return "Due to COVID-19";
+
+
+    } else if (input == "I love <b>ME-My Everything 😎!</b>") {
+        return "Thank You❤️!";
+
+
+    } else if (input == "Heart clicked!") {
+        return "❤️";
+
+
+    } else if (input == "goodbye") {
+            return "Talk to you later😊!";
+
+
+    } else {
+        return "Thank You❤️! See you Again!!";
+    }
+
+
+} 
+
+   
+
